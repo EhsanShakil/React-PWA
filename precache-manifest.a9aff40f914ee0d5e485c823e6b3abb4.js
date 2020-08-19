c3c79a5dd4ec7548eb58bcda726bbff48bb1cba9
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-19 21:27:14 +0500

    Updates

---
+++ precache-manifest.a9aff40f914ee0d5e485c823e6b3abb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaf67729cb750827803aac426f8d107",
+    "url": "/React-PWA/index.html"
+  },
+  {
+    "revision": "ed7829d4f9e2280bc6ca",
+    "url": "/React-PWA/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b5ea13b18639b5c5d67",
+    "url": "/React-PWA/static/js/2.67685b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-PWA/static/js/2.67685b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7829d4f9e2280bc6ca",
+    "url": "/React-PWA/static/js/main.6de1fbbe.chunk.js"
+  },
+  {
+    "revision": "cef205675c46f567deaa",
+    "url": "/React-PWA/static/js/runtime-main.25523d76.js"
+  }
+]);
